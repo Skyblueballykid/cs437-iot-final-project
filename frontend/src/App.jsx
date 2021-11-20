074da7f5a3b047a83b15e6c61b8d682b5de909b5
Thomas Kalnik <kalnik.thomas@gmail.com>
2021-11-20 00:46:18 -0400

    general layout of ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,19 @@ import React from 'react'
 import { styled } from '@mui/material/styles';
 import { Typography, Box, Paper, Grid, Breadcrumbs, Link } from '@mui/material';
 import Chart from "react-apexcharts";
+import GoogleMapReact from 'google-map-react';
+import { Sparklines, SparklinesLine } from 'react-sparklines';
+
+
+
+const REACT_APP_GOOGLE_MAPS_API_KEY = "AIzaSyAr87Toq4g5_CsPvvggwmXzBYSfTOakZQc"
+const mapProps = {
+  center: {
+    lat:  40.116421,
+    lng: -88.243385
+  },
+  zoom: 10
+};
 
 const Item = styled(Paper)(({ theme }) => ({
   ...theme.typography.body2,
@@ -46,10 +59,90 @@ var tempOptions = {
   stroke: {
     lineCap: "round",
   },
-  labels: ["Progress"]
+  labels: ["Temperature"]
 };
 
 var tempSeries = [67];
+
+var humidityOptions = {
+  chart: {
+    height: 80,
+    type: "radialBar"
+  },
+  
+  series: [67],
+  
+  plotOptions: {
+    radialBar: {
+      hollow: {
+        margin: 15,
+        size: "70%"
+      },
+     
+      dataLabels: {
+        showOn: "always",
+        name: {
+          offsetY: -10,
+          show: true,
+          color: "#888",
+          fontSize: "13px"
+        },
+        value: {
+          color: "#111",
+          fontSize: "30px",
+          show: true
+        }
+      }
+    }
+  },
+
+  stroke: {
+    lineCap: "round",
+  },
+  labels: ["Humidity"]
+};
+
+var humiditySeries = [55];
+
+var windSpeedOptions = {
+  chart: {
+    height: 80,
+    type: "radialBar"
+  },
+  
+  series: [67],
+  
+  plotOptions: {
+    radialBar: {
+      hollow: {
+        margin: 15,
+        size: "70%"
+      },
+     
+      dataLabels: {
+        showOn: "always",
+        name: {
+          offsetY: -10,
+          show: true,
+          color: "#888",
+          fontSize: "13px"
+        },
+        value: {
+          color: "#111",
+          fontSize: "30px",
+          show: true
+        }
+      }
+    }
+  },
+
+  stroke: {
+    lineCap: "round",
+  },
+  labels: ["Wind Speed"]
+};
+
+var windSpeedSeries = [21];
 
 function App() {
   return (
@@ -57,34 +150,75 @@ function App() {
       <Typography variant="h3">Team Thundercats Realtime Weather Detection Final Project</Typography> 
       <br/>
       <br/>
-      <Grid container spacing={2}>
-        <Grid item xs={6}>
-          <Item>Sensor Readings
+      <Grid 
+      container
+      spacing={1}>
+        <br/>
+        <Grid 
+        item
+        direction="column"
+        justifyContent="center"
+        alignItems="center" 
+        xs={6}>
+          <br/>
+          <Item>
+          Sensor Readings
+          <br/>
           <Chart
               options={tempOptions}
               series={tempSeries}
               type="radialBar"
-              width="200"
+              width="170"
             />
+            <br/>
+        <Sparklines data={[5, 10, 5, 20]} limit={5} width={700} height={50} margin={1}>
+          <SparklinesLine color="blue" />
+        </Sparklines>
           <Chart
-              options={tempOptions}
-              series={tempSeries}
+              options={humidityOptions}
+              series={humiditySeries}
               type="radialBar"
-              width="200"
+              width="170"
             />
+        <Sparklines data={[66, 32, 81, 19]} limit={5} width={700} height={50} margin={1}>
+          <SparklinesLine color="blue" />
+        </Sparklines>
            <Chart
-              options={tempOptions}
-              series={tempSeries}
+              options={windSpeedOptions}
+              series={windSpeedSeries}
               type="radialBar"
-              width="200"
+              width="170"
             />
-            </Item>
+          <Sparklines data={[6, 3, 12, 3]} limit={5} width={700} height={50} margin={1}>
+          <SparklinesLine color="blue" />
+        </Sparklines>
+        </Item>
         </Grid>
+        <Grid 
+        item
+        direction="column"
+        justifyContent="center"
+        alignItems="center" 
+        xs={6}>
         <Grid item xs={6}>
-          <Item>Realtime Weather Camera</Item>
+        <Item>Map
+          <div style={{ height: '25vh', width: '100%' }}>
+          <GoogleMapReact
+            bootstrapURLKeys={{ key: REACT_APP_GOOGLE_MAPS_API_KEY }}
+            center={mapProps.center}
+            defaultZoom={mapProps.zoom}
+        ></GoogleMapReact></div>
+          </Item>
         </Grid>
-        <Grid item xs={12}>
-          <Item>Map</Item>
+        <br/>
+        <Grid item xs>
+        <Item>Realtime Weather Camera
+            <br/>
+            <div style={{ height: '40vh', width: '100%' }}>
+            <img src='weather-placeholder.png'></img>
+            </div>
+          </Item>
+        </Grid>
         </Grid>
       </Grid>
     </Box>
